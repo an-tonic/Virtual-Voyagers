a7d61a8b9f32b04fd899c8d2d850a971387232f5
ANikolaou5 <anikolaou5@uclan.ac.uk>
2025-04-24 16:28:10 +0300

    Styling

--- lib/CouponsPage.dart
+++ lib/CouponsPage.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CouponsPage extends StatefulWidget {
@@ -14,19 +13,19 @@ class _CouponsPageState extends State<CouponsPage> {
       "Free entry to the market",
       "1 free entry for an adult to any market before 1 July 2025",
       1000,
-      Icon(Icons.store),
+      Icon(Icons.store, color: Colors.indigo.shade900, size: 30),
     ),
     Coupon(
       "5% discount",
       "5% discount for 1 purchase at the hand made market",
       1500,
-      Icon(Icons.percent),
+      Icon(Icons.percent, color: Colors.indigo.shade900, size: 30),
     ),
     Coupon(
       "1 + 1 for drinks",
       "Buy 1 drink at the cafeteria and get 1 more as a gift",
       2000,
-      Icon(Icons.coffee),
+      Icon(Icons.coffee, color: Colors.indigo.shade900, size: 30),
     ),
   ];
 
@@ -35,23 +34,33 @@ class _CouponsPageState extends State<CouponsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Coupons')),
+      appBar: AppBar(title: const Text('Coupons', style: TextStyle(fontWeight: FontWeight.bold))),
       body: ListView.builder(
         padding: const EdgeInsets.all(8),
         itemCount: coupons.length,
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(
-              coupons[index].title,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            subtitle: Text(coupons[index].description),
-            leading: coupons[index].icon,
-            trailing: OutlinedButton(
-              onPressed: onPressed,
-              style: Theme.of(context).outlinedButtonTheme.style,
-              child: Text("Redeem"),
-            ),
+          return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 3,
+              color: Colors.deepOrange.shade200,
+              child: ListTile(
+                title: Text(
+                  coupons[index].title,
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(color: Colors.indigo.shade900),
+                ),
+                subtitle: Text(coupons[index].description, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                leading: coupons[index].icon,
+                trailing: OutlinedButton(
+                  onPressed: onPressed,
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.indigo.shade900, width: 3),
+                    backgroundColor: Colors.deepOrange.shade400,
+                  ),
+                  child: Text("Redeem", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.indigo.shade900, fontSize: 16)),
+                ),
+              )
           );
         },
       ),
